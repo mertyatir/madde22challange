2515e38f9b1fb7b7c4c7dd6edda3c9180a2b54bc
mertyatir <mertyatir@hotmail.com>
2023-05-06 21:53:15 +0300

    order

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import "./App.css"
-import { ReactComponent as Logo } from "./assets/images/Exclude.svg"
+
+import Header from "./components/Header"
 import FilterSection from "./components/FilterSection"
 import ItemBoxList from "./components/ItemBoxList"
-
 import SelectionMenu from "./components/SelectionMenu"
-
 import { FilterProvider } from "./providers/FilterProvider"
 
 function App() {
   return (
     <div className="App">
       <FilterProvider>
-        <Logo className="App-logo" />
-        <h2 className="heading">ETKİNLİKLER</h2>
+        <Header />
         <SelectionMenu />
         <FilterSection />
         <ItemBoxList />
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import { ReactComponent as Logo } from "../assets/images/Exclude.svg"
+
+const Header = () => {
+  return (
+    <>
+      <Logo className="App-logo" />
+      <h2 className="heading">ETKİNLİKLER</h2>
+    </>
+  )
+}
+
+export default Header
